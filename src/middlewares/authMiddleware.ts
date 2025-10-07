c503214e273d1b35f0ef3557619ba12b389de719
supun prasad <supunprasad009@gmail.com>
2025-10-07 21:27:01 +0530

    wrap authMiddleware with asyncHandler, improved error handling in the authMiddleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,16 +1,24 @@
 import type { Request, Response, NextFunction } from "express";
 import { verifyToken, type accessTokenPayload } from "../utils/jwt.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import AppError from "../utils/AppError.js";
+import { HttpStatus } from "../constant/http.js";
 
-export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+export const authMiddleware = asyncHandler(async(req: Request, res: Response, next: NextFunction)=> {
     const accessToken = req.cookies?.accessToken;
 
     if (!accessToken) {
-        return res.status(401).json({ message: "Unauthorized" });
+        return new AppError(HttpStatus.UNAUTHORIZED, "Access token not found");
     }
 
     const decoded = verifyToken(accessToken);
 
+    if (!decoded) {
+        return new AppError(HttpStatus.UNAUTHORIZED, "Invalid access token");
+    }
+
     req.user = decoded as accessTokenPayload;
     next();
+});
+    
 
-}
