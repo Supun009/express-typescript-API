326b2c1cc56c6abed9902fcc5dd8ed8deb9f4a55
supun prasad <supunprasad009@gmail.com>
2025-10-12 08:32:54 +0530

    added githubworkflow

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -18,6 +18,19 @@ export const authMiddleware = asyncHandler(
       throw new AppError(HttpStatus.UNAUTHORIZED, "Invalid access token");
     }
 
+    // CSRF Protection: For any method that is not 'safe' (GET, HEAD, OPTIONS),
+    // we require a CSRF token to be sent in the headers. This token must match
+    // the one stored in the JWT payload.
+    // const unsafeMethods = ["POST", "PUT", "PATCH", "DELETE"];
+    // if (unsafeMethods.includes(req.method)) {
+    //   const csrfTokenFromHeader = req.headers["x-csrf-token"] as string;
+    //   const csrfTokenFromJwt = decoded.csrfToken;
+
+    //   if (!csrfTokenFromHeader || csrfTokenFromHeader !== csrfTokenFromJwt) {
+    //     throw new AppError(HttpStatus.FORBIDDEN, "Invalid CSRF token");
+    //   }
+    // }
+
     req.user = decoded as accessTokenPayload;
     next();
   },
